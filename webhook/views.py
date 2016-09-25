ce402deaab13b0f4007122df94adf2576189b31b
Cesar Serrano <cesr90@gmail.com>
2016-09-25 00:37:13 -0500

    verify

--- webhook/views.py
+++ webhook/views.py
@@ -6,8 +6,10 @@ from rest_framework import viewsets
 class WebhookView(viewsets.ViewSet):
 
     def list(self, request, format=None):
-        challenge = request.data.get('hub_challenge')
-        verify_token = request.data.get('hub_verify_token')
+        challenge = request.data.get('hub_challenge', 'hub_challenge')
+        verify_token = request.data.get('hub_verify_token', 'hub_verify_token')
 
         if verify_token == 'holamundo':
             return Response(challenge)
+
+        return Response('holamundo')
