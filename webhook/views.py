d9c79906e41050e27e383c92cb9cdeb426d10e2c
Cesar Serrano <cesr90@gmail.com>
2016-09-25 00:46:55 -0500

    verify

--- webhook/views.py
+++ webhook/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 class WebhookView(viewsets.ViewSet):
 
     def list(self, request, format=None):
-        if request.GET.get('hub.mode') and request.GET.get('hub.mode') == 'subscribe' and (
-                    request.GET.get('hub.verify_token') == "holamundo"):
+        if request.GET.get("hub.mode") and request.GET.get("hub.mode") == "subscribe" and (
+                    request.GET.get("hub.verify_token") == "holamundo"):
                 return Response(request.GET.get("hub.challenge"))
         return Response('Error, invalid token')
