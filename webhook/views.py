3597f98677fb1e16af5f675033c4edc6d094adbf
Cesar Serrano <cesr90@gmail.com>
2016-09-25 00:57:33 -0500

    verify

--- webhook/views.py
+++ webhook/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 class WebhookView(viewsets.ViewSet):
 
     def list(self, request, format=None):
+        return Response(request.GET.get("hub.challenge"))
         if request.GET.get("hub.mode") and request.GET.get("hub.mode") == "subscribe" and (
                     request.GET.get("hub.verify_token") == "holamundo"):
                 return Response(request.GET.get("hub.challenge").replace("\"", ""))
